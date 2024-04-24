ed35aff8c3adb6721cd2a0c15b9c53500f892856
Danfeng Hong <hongdanfeng1989@gmail.com>
2024-04-24 12:50:50 +0800

    Update option.py

--- CasFormer/simulation/train_code/option.py
+++ CasFormer/simulation/train_code/option.py
@@ -2,7 +2,7 @@ import argparse
 import template
 
 parser = argparse.ArgumentParser(description="HyperSpectral Image Reconstruction Toolbox")
-parser.add_argument('--template', default='casFormer',
+parser.add_argument('--template', default='HRFT',
                     help='You can set various templates in option.py')
 
 # Hardware specifications
@@ -14,27 +14,24 @@ parser.add_argument('--data_root', type=str, default='../../datasets/', help='da
 #                        help='path of data')
 parser.add_argument('--data_path_cave', default='../../datasets/Train/cave_train/', type=str,
                      help='path of data')
-#parser.add_argument('--data_path_ARAD', default='../../datasets/Test/ARAD_50/', type=str,
-#                      help='path of data')
 # parser.add_argument('--data_path_KAIST', default='../../datasets/Train/kaist_train/', type=str,
 #                     help='path of data')
-parser.add_argument('--mask_path', default='../../datasets/Train/mask_256.mat', type=str,
+parser.add_argument('--mask_path', default='../../datasets/Train/mask_train.mat', type=str,
                     help='path of mask')
 
 # Saving specifications
-parser.add_argument('--outf', type=str, default='./exp/casFormer/', help='saving_path')
+parser.add_argument('--outf', type=str, default='./exp/CasFormer/', help='saving_path')
 
 # Model specifications
-parser.add_argument('--method', type=str, default='casFormer', help='method name')
+parser.add_argument('--method', type=str, default='HRFT', help='method name')
 parser.add_argument('--pretrained_model_path', type=str, default=None, help='pretrained model directory')
 parser.add_argument("--input_setting", type=str, default='H',
                     help='the input measurement of the network: H, HM or Y')
 parser.add_argument("--data_path_test",default='../../datasets/Test/cave_test/', type=str,
                         help='path of data')
-#parser.add_argument("--data_path_test",default='../../datasets/Test/ARAD/', type=str, help='path of data')
-parser.add_argument('--TrainMask_path', default='../../datasets/Train/mask256.mat', type=str,
+parser.add_argument('--TrainMask_path', default='../../datasets/Train/mask_train.mat', type=str,
                     help='path of mask')
-parser.add_argument('--TestMask_path', default='../../datasets/Test/mask256.mat', type=str,
+parser.add_argument('--TestMask_path', default='../../datasets/Test/mask_train.mat', type=str,
                     help='path of mask')
 
 # Training specifications
