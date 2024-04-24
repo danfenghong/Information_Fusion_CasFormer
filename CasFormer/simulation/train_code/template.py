9c1fd2146014712c02208b095be4db95a4bb9336
Danfeng Hong <hongdanfeng1989@gmail.com>
2024-04-24 12:52:40 +0800

    Update template.py

--- CasFormer/simulation/train_code/template.py
+++ CasFormer/simulation/train_code/template.py
@@ -1,5 +1,5 @@
 def set_template(args):
     # Set the templates here
-    if args.template.find('casFormer') >= 0:
+    if args.template.find('HRFT') >= 0:
         args.input_setting = 'H'
         args.input_mask = 'Phi'
